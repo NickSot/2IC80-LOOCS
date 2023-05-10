f5a9325be981e32d127df263c0d76b77f175b1f6
NickSot <nikola.s.sotirov@gmail.com>
2023-05-10 17:43:26 +0300

    initial commit

---
+++ get_rekt/dllmain.cpp
@@ -0,0 +1,25 @@
+﻿// dllmain.cpp : Defines the entry point for the DLL application.
+#include "stdafx.h"
+
+// TODO:
+// - create the hook
+// - locate the physics component of the engine
+// - call the hook on each engine tick
+// - export the binary
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
---
+++ injector/injector.cpp
@@ -0,0 +1,14 @@
+﻿// injector.cpp : Defines the entry point for the console application.
+#include "stdafx.h"
+
+// TODO:
+// - get the PID of the game process
+// - spawn a thread in the game process
+// - load the get_rekt.dll file into the game process from the thread
+// - export the binary
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
