4fc537b014618014291b946d0801f47f20dd4a0b
NickSot <nikola.s.sotirov@gmail.com>
2023-05-12 14:09:36 +0300

    the dll is loaded and performs a test functionality :)

--- get_rekt/dllmain.cpp
+++ get_rekt/dllmain.cpp
@@ -14,10 +14,35 @@ BOOL APIENTRY DllMain( HMODULE hModule,
                        LPVOID lpReserved
 					 )
 {
-	FILE * f;
-	fopen_s(&f, "f.txt", "w");
-	fprintf(f, "This works!");
-	fclose(f);
+	HANDLE hFile = CreateFile(L"./f.txt",
+                       GENERIC_WRITE,
+                       0,
+                       NULL,
+                       CREATE_NEW,
+                       FILE_ATTRIBUTE_NORMAL,
+                       NULL);
+
+	if (hFile == INVALID_HANDLE_VALUE) 
+    { 
+        printf("%d", GetLastError());
+        return FALSE;
+    }
+
+
+	char * DataBuffer = "works!";
+	DWORD bytesWritten = 0;
+
+	BOOL bErrorFlag = WriteFile(hFile, DataBuffer, strlen(DataBuffer), &bytesWritten, NULL);
+
+	if (FALSE == bErrorFlag)
+    {
+        printf("Error writing to the file.\n");
+
+		return FALSE;
+    }
+
+	CloseHandle(hFile);
+
 
 	switch (ul_reason_for_call)
 	{
