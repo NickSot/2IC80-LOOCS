bb262b0441701fe597436b64387bc00fdbe3773d
NickSot <nikola.s.sotirov@gmail.com>
2023-05-25 23:44:06 +0300

    make the toggle buttons work

--- get_rekt/dllmain.cpp
+++ get_rekt/dllmain.cpp
@@ -7,9 +7,45 @@
 
 DWORD thread_id;
 
-#pragma comment(lib, "psapi.lib")
+BYTE * JumpAddress;
+BYTE * PlayerInAir;
 
-BYTE * PlayerJumpAddress;
+WNDPROC OriginalWindowProc;
+
+bool BHopToggle;
+bool Disable;
+
+#pragma comment(lib, "psapi.lib")
+#pragma comment( lib, "user32.lib")
+
+LRESULT CALLBACK TrackKeyPresses(HWND Window, UINT Message, WPARAM wParam, LPARAM lParam) {
+	printf("Key pressed: %d\n", wParam);
+	printf("Disable cheat: %d\n", Disable);
+
+	// the key we're looking for is 'L' = 0x4c || 0x6c
+	if (Message == WM_KEYDOWN) {
+		if (wParam == 0x4c || wParam == 0x6c) {
+			BHopToggle = !BHopToggle;
+		}
+
+		if (wParam == 0x48 || wParam == 0x68) {
+			Disable = true;
+		}
+	}
+	
+	// logic for the BHop machine toggling
+	if (BHopToggle) {
+		if (* PlayerInAir) {
+			*JumpAddress = 0;
+		}
+		else {
+			*JumpAddress = 5;
+		}
+	}
+
+	// return back to the context of the hooked procedure
+	return CallWindowProcA(OriginalWindowProc, Window, Message, wParam, lParam);
+}
 
 VOID WINAPI Play(LPVOID Module) {
 	BYTE * client = NULL;
@@ -21,15 +57,31 @@ VOID WINAPI Play(LPVOID Module) {
 		engine = (BYTE *) GetModuleHandle(L"engine.dll");
 	}
 
-	// jump offset
-	BYTE * JumpAddress = client + 0x4F5D24;
-	*JumpAddress = 5;
+	BHopToggle = false;
+	Disable = false;
 
-	// CALL THIS FUNCTION HERE ALWAYS!!!
+	// jump offset
+	JumpAddress = client + 0x4F5D24;
+	// player in air state offset
+	PlayerInAir = client + 0x4A4078;
+
+	// get the main game window
+	HWND GameWindow = FindWindowA("Valve001", 0);
+
+	// set the hook for the main window
+	OriginalWindowProc = (WNDPROC)SetWindowLongPtr(GameWindow, GWLP_WNDPROC, (LONG_PTR)TrackKeyPresses);
+
+	while (!Disable) {
+		Sleep(1000);
+	}
+
+	// uninject the DLL
+	SetWindowLongPtr(GameWindow, GWLP_WNDPROC, (LONG_PTR)OriginalWindowProc);
 	FreeLibraryAndExitThread((HMODULE)Module, 0);
 }
-
+ 
 void LoopThread() {
+	// create a thread to do the heavy work
 	HANDLE LoopThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Play, NULL, NULL, &thread_id);
 
 	if (LoopThread != NULL) {
@@ -37,32 +89,11 @@ void LoopThread() {
 	}
 }
 
-void Detatch() {
-	HANDLE LoopThread = OpenThread(THREAD_TERMINATE, FALSE, thread_id);
-
-	if (LoopThread != NULL) {
-		CloseHandle(LoopThread);
-	}
-}
-
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{	
-	switch (ul_reason_for_call)
+BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
 	{
-	case DLL_PROCESS_ATTACH:
-	case DLL_THREAD_ATTACH:
 		LoopThread();
-		break;
-
-	case DLL_THREAD_DETACH:
-		Detatch();
-		break;
-
-	case DLL_PROCESS_DETACH:
-		break;
 	}
 
 	return TRUE;
